6d8d8d76cb3cb7bc3103c769bb25e3803c0daa06
Emma Dedmond <emmadedmond@yahoo.com>
2019-05-05 16:36:15 -0400

    added readme and fixed logo link

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Google-Books-Search
 An application that allows you to view and save google books.
++
+To use, simply type into the search bar, a book title, or author name (or both) and click 'submit'.
+
+Once search results have appeared you can save them to the database by clicking "save".
+
+You can also click on the "save" button in the navbar to be taken to a page that lists all books that have been saved to the database. Each book should appear with a "delete" button that will remove the book from the database.
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function Navbar (props) {
     const path = props.history.location.pathname;
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Google Books Search</a>
+            <a className="navbar-brand" href="/">Google Books Search</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
